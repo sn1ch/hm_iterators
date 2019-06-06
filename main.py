9026dc9894f96aae2692eda3c1cdeb1b4ea84373
sn1ch <sn1ch@list.ru>
2019-06-06 20:05:51 +0300

    git vperet!

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+from pprint import pprint
+
+
+class My_iterator:
+    def __init__(self, path):
+        with open(path, encoding='utf8') as file:
+            data = json.load(file)
+            self.count = -1
+            self.data = data
+            self.result = []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        URL = 'https://en.wikipedia.org/wiki/'
+        if self.count == len(self.data) - 1:
+            raise StopIteration
+        self.count += 1
+        country = str(self.data[self.count]['name']['common'])
+        data = country + ' - ' + URL + country.replace(" ", "_")
+        self.result.append(data)
+        with open('data/countries_and_link.json', 'w', encoding='utf8') as file:
+            json.dump(self.result, file, ensure_ascii=False, indent=4, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+    countries = My_iterator('data/countries.json')
+
+    for item in countries:
+        item
+    pprint(countries.result)
---
+++ main2.py
@@ -0,0 +1,30 @@
+import json
+from pprint import pprint
+import hashlib
+
+
+class My_iterator_hash:
+    def __init__(self, path):
+        with open(path, encoding='utf8') as file:
+            data = json.load(file)
+            self.count = -1
+            self.data = data
+            self.result = []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count == len(self.data) - 1:
+            raise StopIteration
+        self.count += 1
+        byte = str.encode(self.data[self.count])
+        hash = hashlib.md5(byte)
+        return hash.digest()
+
+
+if __name__ == '__main__':
+    countries = My_iterator_hash('data/countries_and_link.json')
+    for coutry in countries:
+        print(coutry)
+
